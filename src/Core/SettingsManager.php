9d40344e10e913905182cde49e45cc582af6247b
Cotton <yahyadard@gmail.com>
2025-11-11 21:34:25 +0100

    feat: Add AJAX handler for field mapping suggestions retrieval

--- src/Core/SettingsManager.php
+++ src/Core/SettingsManager.php
@@ -113,6 +113,9 @@ class SettingsManager {
 		add_action( 'wp_ajax_ghl_get_logs', [ $this, 'handle_get_logs' ] );
 		add_action( 'wp_ajax_ghl_delete_old_logs', [ $this, 'handle_delete_old_logs' ] );
 		add_action( 'wp_ajax_ghl_clear_all_logs', [ $this, 'handle_clear_all_logs' ] );
+
+		// Field Mapping Suggestions (delegated to AjaxHandler)
+		add_action( 'wp_ajax_ghl_crm_get_field_suggestions', [ $this, 'handle_get_field_suggestions' ] );
 	}
 
 	/**
@@ -2136,6 +2139,16 @@ class SettingsManager {
 	public function handle_clear_all_logs(): void {
 		// Delegate to AjaxHandler (nonce and permissions checked there)
 		AjaxHandler::clear_all_logs();
+	}
+
+	/**
+	 * Handle get field suggestions AJAX request
+	 *
+	 * @return void
+	 */
+	public function handle_get_field_suggestions(): void {
+		// Delegate to AjaxHandler (nonce and permissions checked there)
+		AjaxHandler::get_field_suggestions();
 	}
 
 	/**
