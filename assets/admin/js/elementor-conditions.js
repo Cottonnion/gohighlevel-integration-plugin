3db22b871cae31ec07b8a44413f613c713386f3d
Yahya Eddaqqaq <yahyadard@gmail.com>
2026-03-08 11:55:38 +0000

    Refactor elementor-conditions.js for improved readability by standardizing formatting and enhancing code structure

--- assets/admin/js/elementor-conditions.js
+++ assets/admin/js/elementor-conditions.js
@@ -2,18 +2,21 @@
  * GoHighLevel Elementor Conditions
  * Handles dynamic tag loading in Elementor editor
  */
-(function($) {
-    'use strict';
+(function ($) {
+  "use strict";
 
-    // Wait for Elementor to be ready
-    $(window).on('elementor:init', function() {
-        // Tags are already loaded via PHP localization
-        // This file is here for future enhancements if needed
-        
-        // Debug log (can be removed in production)
-        if (typeof ghlElementorData !== 'undefined') {
-            console.log('GHL Elementor Conditions loaded with', Object.keys(ghlElementorData.availableTags).length, 'tags');
-        }
-    });
+  // Wait for Elementor to be ready
+  $(window).on("elementor:init", function () {
+    // Tags are already loaded via PHP localization
+    // This file is here for future enhancements if needed
 
+    // Debug log (can be removed in production)
+    if (typeof ghlElementorData !== "undefined") {
+      console.log(
+        "GHL Elementor Conditions loaded with",
+        Object.keys(ghlElementorData.availableTags).length,
+        "tags",
+      );
+    }
+  });
 })(jQuery);
